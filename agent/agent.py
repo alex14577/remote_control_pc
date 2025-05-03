a459ed46be53d24cd140780457c158ca707c99d3
Александр Чесноков <alexanc40@gmail.com>
2025-05-03 18:09:23 +0300

    aaa

--- agent/agent.py
+++ agent/agent.py
@@ -7,19 +7,19 @@ from typing import Callable, Awaitable
 
 from agent.logger import Logger
 
-logger = Logger().Get("agent")
 
 class Agent:
     def __init__(self):
         self.handlers: dict[str, Callable[[dict, websockets.WebSocketServerProtocol], Awaitable[None]]] = {}
         self.connected_clients = set()
+        self._logger = Logger().Get("agent")
 
     def register(self, command_name: str):
         def decorator(fn: Callable[[dict, websockets.WebSocketServerProtocol], Awaitable[None]]):
             if command_name in self.handlers:
-                logger.warning(f"Command '{command_name}' is already registered. Overwriting.")
+                self._logger.warning(f"Command '{command_name}' is already registered. Overwriting.")
             else:
-                logger.info(f"Registered command: {command_name}")
+                self._logger.info(f"Registered command: {command_name}")
             self.handlers[command_name] = fn
             return fn
         return decorator
@@ -30,10 +30,10 @@ class Agent:
         if handler:
             await handler(message, websocket)
         else:
-            logger.error(f"\u26a0\ufe0f Unknown command: {command}")
+            self._logger.error(f"\u26a0\ufe0f Unknown command: {command}")
 
     async def handler(self, websocket: websockets.WebSocketServerProtocol):
-        logger.info("\U0001f91d Client connected")
+        self._logger.info("\U0001f91d Client connected")
         self.connected_clients.add(websocket)
         try:
             async for message in websocket:
@@ -41,14 +41,14 @@ class Agent:
                     command = json.loads(message)
                     await self.handle(command, websocket)
                 except Exception as e:
-                    logger.error(f"\u26a0\ufe0f Error handling command: {e}")
+                    self._logger.error(f"\u26a0\ufe0f Error handling command: {e}")
         except websockets.ConnectionClosed:
-            logger.info("❌ Client disconnected")
+            self._logger.info("❌ Client disconnected")
         finally:
             self.connected_clients.remove(websocket)
 
     async def run(self, host: str, port: int):
-        logger.info(f"\U0001f680 Agent listening on {host}:{port}")
+        self._logger.info(f"\U0001f680 Agent listening on {host}:{port}")
         async with websockets.serve(self.handler, host, port):
             await asyncio.Future()  # run forever
 
